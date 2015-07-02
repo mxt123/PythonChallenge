790925bbd47081c5bcbd92c7471f2c05d781fc89
Matt Toseland <m.toseland@validus-ivc.co.uk>
2015-07-02 09:40:28 +0100

    wip

--- 0.py
+++ 0.py
@@ -1,3 +1,6 @@
 import webbrowser
 
 webbrowser.open('http://www.pythonchallenge.com/pc/def/' + str(2**38)+ '.html')
+
+#http://www.pythonchallenge.com
+#http://www.pythonchallenge.com/pc/def/map.html
