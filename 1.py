50f64c1ac06fccf12ec25817283570e16c8e10a4
= <=>
2015-07-02 10:14:15 +0100

    wip

--- 1.py
+++ 1.py
@@ -1,3 +1,5 @@
+#http://www.pythonchallenge.com/pc/def/map.html
+
 #!/usr/bin/python
 #text = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 #mapped = ""
